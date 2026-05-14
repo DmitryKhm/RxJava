be50bae9dd09d10125ab96be344b2fcfd5a8e994
DmitryKhm <71850201+DmitryKhm@users.noreply.github.com>
2026-05-14 18:42:25 +0300

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+SRC_MAIN=src/main/java
+SRC_TEST=src/test/java
+OUT=out
+
+echo "=== Compiling ==="
+mkdir -p $OUT
+find $SRC_MAIN $SRC_TEST -name "*.java" | xargs javac --release 21 -d $OUT
+
+echo ""
+echo "=== Running Tests ==="
+java -cp $OUT rx.RxTests
+
+echo ""
+echo "=== Running Demo ==="
+java -cp $OUT demo.Demo
